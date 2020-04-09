5336485439c1f3adf4a50694f28046ba9b2443d7
karlssonr <karlssonr1989@gmail.com>
2020-04-09 10:48:17 +0200

    translating strings

--- app/src/main/java/com/example/map19resorces/MainActivity.kt
+++ app/src/main/java/com/example/map19resorces/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.map19resorces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var greetingTextView : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        greetingTextView = findViewById(R.id.textView)
+
+    }
+
+    fun buttonPressed(view : View) {
+
+    greetingTextView.text = getString(R.string.thank_you)
+
     }
 }
